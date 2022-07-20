c2e2c798913d0ce032c57d4c08f6eb17606d6d40
specnix <spec.itx@gmail.com>
2022-07-20 09:15:48 +0300

    Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/info.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import java.util.Date;
+
+public class info {
+    public static void main(String[] args) {
+
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
