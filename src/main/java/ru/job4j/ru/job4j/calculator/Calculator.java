0c6d5a85914a9fc627c20d6ac79da2afa7fab98d
specnix <spec.itx@gmail.com>
2022-07-23 12:06:46 +0300

    Арифметические операции. [#185]

--- src/main/java/ru/job4j/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/ru/job4j/calculator/Calculator.java
@@ -2,8 +2,7 @@ package ru.job4j.ru.job4j.calculator;
 
 public class Calculator {
 
-    @SuppressWarnings("checkstyle:MethodParamPad")
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int one = 1;
         int two = 2;
         int six = 6;
