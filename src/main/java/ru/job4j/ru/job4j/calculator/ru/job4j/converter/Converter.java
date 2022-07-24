17971903ed0d8b2cea6fe122316642514668b19c
specnix <spec.itx@gmail.com>
2022-07-24 11:09:11 +0300

    5. Конвертер валюты [#41583]

---
+++ src/main/java/ru/job4j/ru/job4j/calculator/ru/job4j/converter/Converter.java
@@ -0,0 +1,19 @@
+package ru.job4j.ru.job4j.calculator.ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value){
+        float rls = value / 70;
+        return rls;
+    }
+    public static float rubleToDollar(float value){
+        float rls = value / 60;
+        return rls;
+    }
+    public static void main(String[] args){
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("Ваша сумма в евро: "+ euro + '€');
+        float dollar = Converter.rubleToDollar(120);
+        System.out.println("Ваша сумма в долларах: " + dollar + '$');
+    }
+}
