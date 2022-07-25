e5beaf7501a67b7e4aedbd9ec43a6886f82f29ba
specnix <spec.itx@gmail.com>
2022-07-25 07:45:17 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/ru/job4j/calculator/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j.ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double x = (height - 100) * 1.15;
+        return x;
+    }
+
+    public static double womanWeight(short height) {
+        double x = (height - 110) * 1.15;
+        return x;
+    }
+
+    public static void main(String[] args) {
+        short heightMen = 175;
+        double man = Fit.manWeight(heightMen);
+        System.out.println("Ваш идеальный мужской вес составляет " + man);
+        short heightWoman = 175;
+        double woman = Fit.womanWeight(heightWoman);
+        System.out.println("Ваш идеальный женский вес составляет " + woman);
+    }
+}
--- src/main/java/ru/job4j/ru/job4j/calculator/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/ru/job4j/calculator/ru/job4j/converter/Converter.java
@@ -2,17 +2,17 @@ package ru.job4j.ru.job4j.calculator.ru.job4j.converter;
 
 public class Converter {
 
-    public static float rubleToEuro(float value){
+    public static float rubleToEuro(float value) {
         float rls = value / 70;
         return rls;
     }
-    public static float rubleToDollar(float value){
+    public static float rubleToDollar(float value) {
         float rls = value / 60;
         return rls;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        System.out.println("Ваша сумма в евро: "+ euro + '€');
+        System.out.println("Ваша сумма в евро: " + euro + '€');
         float dollar = Converter.rubleToDollar(120);
         System.out.println("Ваша сумма в долларах: " + dollar + '$');
     }
