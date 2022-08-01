84420c11285269b9e1e90303f7f82e286eb2372f
specnix <spec.itx@gmail.com>
2022-08-01 05:03:26 +0300

    0. Что такое тестирование [#159431]

---
+++ src/main/java/ru/job4j/converter/Convertert.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+public class Convertert {
+    public static float rubleToEuro(float value) {
+        float rls = value / 70;
+        return rls;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rls = value / 60;
+        return rls;
+    }
+
+    public static void main(String[] args) {
+        float inEuro = 140;
+        float expectedEuro = 2;
+        float outEuro = Convertert.rubleToEuro(inEuro);
+        boolean passedEuro = expectedEuro == outEuro;
+        System.out.println("140 rubles are 2. Test result : " + passedEuro);
+
+        float inDollar = 120;
+        float expectedDollar = 2;
+        float outDollar = Convertert.rubleToDollar(inDollar);
+        boolean passedDollar = expectedDollar == outDollar;
+        System.out.println("120 rubles are 2. Test result : " + passedDollar);
+    }
+}
