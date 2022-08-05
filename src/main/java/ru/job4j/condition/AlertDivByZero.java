4880ff8bcd70eae81522bc531d8e5585019d0675
specnix <spec.itx@gmail.com>
2022-08-05 05:35:31 +0300

    4.1. Операторы ветвлений [#3611]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class AlertDivByZero {
     public static void main(String[] args) {
-        possibleDiv(1);
+        possibleDiv(0);
         possibleDiv(10);
         possibleDiv(-5);
     }
@@ -10,7 +10,7 @@ public class AlertDivByZero {
     public static void possibleDiv(int number) {
         if (number < 0) {
             System.out.println("This is negative number: " + number);
-             if (number == 0) {
+            if (number == 0) {
                 System.out.println("This is negative number: " + number);
             }
         }
