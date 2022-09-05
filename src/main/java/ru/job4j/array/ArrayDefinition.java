0bf29268b00414a998e4bfdcaa202576b7cc0ae5
specnix <spec.itx@gmail.com>
2022-09-05 20:16:07 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println(ages.length);
-        String[] surnames = new String[100500];
-        System.out.println(surnames.length);
-        float[] prices = new float[40];
-        System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] =  "Petr";
+        names[1] =  "Ivan";
+        names[2] =  "Sergey";
+        names[3] =  "Nikolay";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
