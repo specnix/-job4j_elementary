2dc661628439b8e30c3a77b0b3dff5badd82e5e0
specnix <spec.itx@gmail.com>
2022-09-26 08:14:21 +0300

    6.4.0. Слово начинается с ... [#41585 #345420]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,9 +5,9 @@ public class ArrayChar {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
             if (word[i] != pref[i]) {
-            result = false;
-            break;
-          }
+                result = false;
+                break;
+            }
         }
         return result;
     }
