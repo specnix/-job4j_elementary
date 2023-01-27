ce9938bc8d66e9498a31b598c7480cf22822d5cb
specnix <spec.itx@gmail.com>
2023-01-27 08:51:09 +0300

    6.6.4. Двухмерный массив. Таблица умножения. [#33491 #384884]

--- src/main/java/ru/job4j/array/MatrixSum.java
+++ src/main/java/ru/job4j/array/MatrixSum.java
@@ -1,13 +1,24 @@
 package ru.job4j.array;
 
 public class MatrixSum {
-    public static int sum(int[][] array) {
-        int rsl = 0;
+    public  static void find(int[][] array, int el) {
+        System.out.println("Find el : " + el);
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                rsl += array[row][cell];
+                int val = array[row][cell];
+                if (val == el) {
+                    System.out.println("row : " + row + ", cell : " + cell);
+                }
             }
         }
-        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {3, 2, 1},
+                {2, 3, 1}
+        };
+        MatrixSum.find(array, 3);
     }
 }
